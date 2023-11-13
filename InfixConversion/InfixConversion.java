3ac0bf1cb545bc9e113b50a765c236dddf7adaec
unknown <chronobait@gmail.com>
2023-11-13 13:10:37 +0800

    Removed Prefix, Added Evaluation

--- InfixConversion/InfixConversion.java
+++ InfixConversion/InfixConversion.java
@@ -2,136 +2,24 @@
 
 import java.util.*;
 
-class Conversion {
-    Conversion(String oldInfix) {
-        String infix = oldInfix;
-    }
-
-    public String getPostfix(String infix) {
-        String result = "";
-        char c;
-        Stack<Character> operator = new Stack<>();
-
-        for (int i = 0; i < infix.length(); ++i) {
-            c = infix.charAt(i);
-
-            if (Character.isDigit(c) || Character.isLetter(c)) {
-                result += c;
-            } else if (c == '(') {
-                operator.push(c);
-            } else if (c == ')') {
-                while (operator.peek() != '(') {
-                    result += operator.pop();
-                }
-                operator.pop();
-            } else {
-                while (!operator.isEmpty() && !(operator.peek() == '(')
-                        && checkPrecedence(c) <= checkPrecedence(operator.peek())) {
-                    result += operator.pop();
-                }
-                operator.push(c);
-            }
-        }
-
-        while (!operator.isEmpty()) {
-            result += operator.pop();
-        }
-
-        return result;
-    }
-
-    public String getPrefix(String infix) {
-        char c;
-        Stack<Character> operators = new Stack<>();
-        Stack<String> operands = new Stack<>();
-
-        for (int i = 0; i < infix.length(); i++) {
-            c = infix.charAt(i);
-
-            if (c == '(') {
-                operators.push(c);
-            } else if (c == ')') {
-                while (!operators.empty() && operators.peek() != '(') {
-                    String operand1 = operands.peek();
-                    operands.pop();
-                    String operand2 = operands.peek();
-                    operands.pop();
-                    char operator = operators.peek();
-                    operators.pop();
-                    String temp = operator + operand2 + operand1;
-                    operands.push(temp);
-                }
-                operators.pop();
-            } else if (!isOperator(c)) {
-                operands.push(c + "");
-            } else {
-                while (!operators.empty() &&
-                        checkPrecedence(c) <= checkPrecedence(operators.peek())) {
-
-                    String operand1 = operands.peek();
-                    operands.pop();
-                    String operand2 = operands.peek();
-                    operands.pop();
-                    char operator = operators.peek();
-                    operators.pop();
-                    String temp = operator + operand2 + operand1;
-                    operands.push(temp);
-                }
-                operators.push(c);
-            }
-        }
-
-        while (!operators.empty()) {
-            String operand1 = operands.peek();
-            operands.pop();
-            String operand2 = operands.peek();
-            operands.pop();
-            char operator = operators.peek();
-            operators.pop();
-            String temp = operator + operand2 + operand1;
-            operands.push(temp);
-        }
-
-        return operands.peek();
-    }
-
-    public static int checkPrecedence(char c) {
-        if (c == '+' || c == '-')
-            return 1;
-        if (c == '*' || c == '/' || c == '%')
-            return 2;
-        if (c == '^')
-            return 3;
-        return 0;
-    }
-
-    public static boolean isOperator(char c) {
-        if (c == '+' || c == '-' || c == '*' || c == '/') {
-            return true;
-        }
-
-        return false;
-    }
-}
-
 class InfixConversion {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         // (2 - 4 / 4 ) * (8 / 2 - 3)
         // (6 - 4 + 2) / 1 * (10/5+(4*1))
-        // x+y*z/w+u
 
-        System.out.print("Enter equation: ");
-        String infix = scan.nextLine();
+        String infix = "10 / (3 + 2)";
+        System.out.println("Enter equation: " + infix);
+        // String infix = scan.nextLine();
 
         infix = infix.replaceAll(" ", "");
 
         if (checkParenthesis(infix) == true) {
             Conversion convert = new Conversion(infix);
 
-            System.out.println("Postfix: " + convert.getPostfix(infix));
-            System.out.println("Prefix: " + convert.getPrefix(infix));
+            System.out.println("Postfix: " + convert.getPostfix());
+            System.out.println("Value: " + convert.evalValue(convert.getPostfix()));
         } else {
             System.out.println("Missing Parenthesis!");
         }
@@ -149,5 +37,128 @@ class InfixConversion {
         }
 
         return parenthesisCnt % 2 == 0;
+    }
+}
+
+class Conversion {
+    Conversion(String infix) {
+        this.infix = infix;
+    }
+
+    String infix;
+
+    public String getPostfix() {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> operator = new Stack<>();
+
+        for (int i = 0; i < infix.length(); i++) {
+            StringBuilder multiDigit = new StringBuilder();
+            char c = infix.charAt(i);
+
+            if (Character.isDigit(c)) {
+                while (i < infix.length() && Character.isDigit(infix.charAt(i))) {
+                    multiDigit.append(infix.charAt(i));
+                    i++;
+                }
+                result.append(multiDigit).append(" ");
+                i--;
+            } else if (c == '(') {
+                operator.push(c);
+            } else if (c == ')') {
+                while (operator.peek() != '(') {
+                    result.append(operator.pop()).append(" ");
+                }
+                operator.pop();
+            } else {
+                if (operator.isEmpty() || operator.peek() == '(') {
+                    operator.push(c);
+                } else {
+                    while (!operator.isEmpty() && checkPrecedence(c) <= checkPrecedence(operator.peek())) {
+                        result.append(operator.pop()).append(" ");
+                    }
+                    operator.push(c);
+                }
+            }
+        }
+
+        while (!operator.isEmpty()) {
+            result.append(operator.pop()).append(" ");
+        }
+
+        return result.toString();
+    }
+
+    public double evalValue(String expression) {
+        Stack<String> operands = new Stack<>();
+        ArrayList<String> solution = new ArrayList<>();
+        ArrayList<String> operand = new ArrayList<>();
+        String[] operators = expression.split(" ");
+        double value = 0;
+
+        for (int i = 0; i < operators.length; i++) {
+            solution.add(operators[i]);
+            if (isOperator(operators[i]) == true) {
+                double operand2 = Double.parseDouble(operands.pop());
+                double operand1 = Double.parseDouble(operands.pop());
+
+                switch (operators[i]) {
+                    case "+":
+                        value = operand1 + operand2;
+                        System.out.println(operand1 + " + " + operand2);
+                        break;
+                    case "-":
+                        value = operand1 - operand2;
+                        System.out.println(operand1 + " - " + operand2);
+                        break;
+                    case "*":
+                        value = operand1 * operand2;
+                        System.out.println(operand1 + " * " + operand2);
+                        break;
+                    case "/":
+                        try {
+                            value = operand1 / operand2;
+                            System.out.println(operand1 + " / " + operand2);
+                        } catch (Exception e) {
+                            System.out.println("Cannot divide by 0!");
+                        }
+                        break;
+                    case "%":
+                        value = operand1 % operand2;
+                        System.out.println(operand1 + " % " + operand2);
+                        break;
+                    case "^":
+                        value = Math.pow(operand1, operand2);
+                        System.out.println(operand1 + " ^ " + operand2);
+                        break;
+                }
+                operands.push(String.valueOf(value));
+            } else {
+                operands.push(operators[i]);
+                operand.add(operators[i]);
+            }
+            // System.out.println(i + " " + solution.toString());
+        }
+
+        return value;
+    }
+
+    public static int checkPrecedence(char c) {
+        if (c == '+' || c == '-')
+            return 1;
+        if (c == '*' || c == '/' || c == '%')
+            return 2;
+        if (c == '^')
+            return 3;
+        return 0;
+    }
+
+    public static boolean isOperator(String c) {
+        String[] operators = { "+", "-", "*", "/", "%", "^" };
+        for (String operator : operators) {
+            if (c.equals(operator)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
